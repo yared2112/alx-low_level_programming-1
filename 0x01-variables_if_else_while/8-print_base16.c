4ddb561fef519b8a1245894892376a3a74ba563a
Nardos <narditek1@gmail.com>
2022-03-12 03:30:36 +0900

    We do to hoaadssdffcs

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,20 +1,23 @@
-#include <stdio.h> 
- 
-/** 
-  * main - Prints a hexadecimal string 
-  * 
-  * Return: Always (Success) 
-  */ 
-int main(void) 
-{ 
- char c; 
- 
- for (c = '0'; c <= '9'; c++) 
- { 
-  putchar(c); 
- } 
- 
- for (c = 'a'; c <= 'f'; c++) 
- { 
-  putchar(c); 
- }
+#include <stdio.h>
+/**
+ *  main - Entery Point
+ *  Return: 0(Success)
+ */
+int main(void)
+{
+  char a, b;
+
+  b = '0';
+  while (b <= '9')
+  {
+    putchar(b);
+    b++;
+  }
+  for (a = 'a'; a <= 'f'; a++)
+  {
+    putchar(a);
+  }
+
+  putchar('\n');
+  return (0);
+}
