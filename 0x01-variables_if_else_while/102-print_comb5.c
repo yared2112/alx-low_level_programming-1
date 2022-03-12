f3bc5063f60e68a2ae06903afb78a2065d5087e8
narditek12 <51337463+narditek12@users.noreply.github.com>
2022-03-12 15:08:22 +0300

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,50 +1,35 @@
-#include <stdio.h>
-#include <stdlib.h>
-/**
- * main - main block
- * Description: Write a program that prints all possible
- * different combinations of two digits
- * Return: 0
- */
+#include <stdio.h> 
+/** * main - main block * Description: Print all possible combinations of two two-digit numbers. * Numbers should range from 0 to 99. * The two numbers should be separated by a space. * All numbers should be printed with two digits. 1 should be printed as 01. * Combination of numbers must be separated by a comma followed by a space. * Combinations of numbers should be printed in ascending order. * 00 01 and 01 00 are considered as the same combination. * You can only use putchar to print to console. * You can only use putchar up to 8 times. * You are not allowed to use any variable of type char. * Return: 0 */
 int main(void)
 {
-int c;
-int d;
-int e;
-int f = 0;
-
-while (f < 10)
+int i, j;int a, b, c, d; 
+for (i = 0; i < 100; i++)
 {
-	e = 0;
-	while (e < 10)
-	{
-		d = 0;
-		while (d < 10)
-		{
-			c = 0;
-			while (c < 10)
-			{
-				if (!(f == c && e == d))
-				{
-					putchar('0' + f);
-					putchar('0' + e);
-					putchar(' ');
-					putchar('0' + d);
-					putchar('0' + c);
-					if (!(f + e == 18 && c + d == 17 && d == 9))
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-				c++;
-			}
-			d++;
-		}
-		e++;
-	}
-	f++;
+a = i / 10;
+ /* doubles fnum */
+b = i % 10;
+ /* singles fnum */ 
+for (j = 0; j < 100; j++)
+{
+c = j / 10;
+ /* doubles snum */
+d = j % 10;
+ /* singles snum */
+if (a < c || (a == c && b < d))
+{
+putchar(a + '0');
+putchar(b + '0');
+putchar(32);
+putchar(c + '0');
+putchar(d + '0'); 
+if (!(a == 9 && b == 8))
+{
+putchar(44);
+putchar(32);
 }
-putchar('\n');
-return (0);
+}
+}
+}
+putchar(10);
+return(0);
 }
